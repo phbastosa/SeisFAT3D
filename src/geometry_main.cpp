c7b0d29a1c10465575c1ca84cabfeef49c243d5f
phbastosa <pbastos@id.uff.br>
2023-04-10 17:56:43 -0300

    geometry main adjusted

--- src/geometry_main.cpp
+++ src/geometry_main.cpp
@@ -4,54 +4,34 @@
 
 int main(int argc, char **argv)
 {
-    Geometry * shots[] =
-    {
-        new Regular(),
-        new Circular(),
-        new Streamer()
-    }; 
-
-    Geometry * nodes[] =
-    {
-        new Regular(),
-        new Circular(),
-        new Streamer()
-    }; 
+    Geometry * shots[] = { new Regular(), new Circular(), new Streamer() }; 
+    Geometry * nodes[] = { new Regular(), new Circular(), new Streamer() }; 
 
     std::string file = std::string(argv[1]);
 
     int type = std::stoi(catch_parameter("geometry_type", file));
 
-    shots[0]->set_geometry(file, "shots");
-    // nodes[0]->set_geometry(file, "nodes");
+    switch (type)
+    {
+        case 0:
+            shots[0]->set_geometry(file, "shots");
+            nodes[0]->set_geometry(file, "nodes");
+            break;
 
-    // switch (type)
-    // {
-    // case 0:
+        case 1:
+            shots[1]->set_geometry(file, "shots");
+            nodes[0]->set_geometry(file, "nodes");
+            break;
 
+        case 2:
+            shots[0]->set_geometry(file, "shots");
+            nodes[2]->set_geometry(file, "nodes");
+            break;
 
-    //     break;
-
-    // case 1:
-
-    //     shots[1]->set_geometry(file, "shots");
-    //     nodes[0]->set_geometry(file, "nodes");
-
-    //     break;
-
-    // case 2:
-
-    //     shots[0]->set_geometry(file, "shots");
-    //     nodes[2]->set_geometry(file, "nodes");
-
-    //     break;
-
-    // default:
-
-    //     throw std::invalid_argument("\033[31mGeometry error: wrong type!\033[0;0m");          
-
-    //     break;
-    // }
+        default:
+            throw std::invalid_argument("\033[31mGeometry error: wrong type!\033[0;0m");          
+            break;
+    }
 
     return 0;
 }
