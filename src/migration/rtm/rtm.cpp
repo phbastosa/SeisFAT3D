5f308a9fda271909acb4140120013ce33b2ba91a
phbastosa <pbastos@id.uff.br>
2023-10-11 14:02:02 -0300

    reformulating

---
+++ src/migration/rtm/rtm.cpp
@@ -0,0 +1,6 @@
+# include "rtm.hpp"
+
+void RTM::set_name()
+{
+    name = "Kirchhoff migration is active!";
+}
---
+++ src/migration/rtm/rtm.hpp
@@ -0,0 +1,11 @@
+# ifndef RTM_HPP
+# define RTM_HPP
+
+# include "../migration.hpp"
+
+class RTM : public Migration
+{
+    void set_name();
+};
+
+# endif
