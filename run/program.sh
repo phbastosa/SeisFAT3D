3ca3df4781a11f5ac3fcb06ceee20c0efcdf48bf
phbastosa <phbastosa@gmail.com>
2024-12-02 11:35:33 -0300

    update program

--- run/program.sh
+++ run/program.sh
@@ -148,12 +148,12 @@ case "$1" in
 
 -test_inversion) 
 
-    # python3 -B ../tests/inversion/generate_models.py
-    # python3 -B ../tests/inversion/generate_geometry.py
+    python3 -B ../tests/inversion/generate_models.py
+    python3 -B ../tests/inversion/generate_geometry.py
 
-    # ./../bin/modeling.exe ../tests/inversion/parameters_obsData.txt
+    ./../bin/modeling.exe ../tests/inversion/parameters_obsData.txt
 
-    # ./../bin/inversion.exe ../tests/inversion/parameters_least_squares.txt
+    ./../bin/inversion.exe ../tests/inversion/parameters_least_squares.txt
     ./../bin/inversion.exe ../tests/inversion/parameters_adjoint_state.txt
 
     python3 -B ../tests/inversion/generate_figures.py
